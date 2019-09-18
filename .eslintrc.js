9dad733cc160622405a1c690c5e8911c3b53f2e3
Lineu Pastorelli <lineualberto@gmail.com>
2019-09-18 09:31:00 -0300

    project completed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    DEV: 'readonly',
+    __DEV__: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
@@ -19,14 +18,13 @@ module.exports = {
   },
   plugins: ['react', 'prettier'],
   rules: {
-    'react/static-property-placement': 'off',
-    'react/jsx-props-no-spreading': 'off',
-
     'prettier/prettier': 'error',
-    'react/state-in-constructor': 'off',
-    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.js']}],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
     'import/prefer-default-export': 'off',
-    'no-param-reassign': 'off',
-    'no-console': ['error', {allow: ['tron']}],
   },
 };
